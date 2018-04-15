059ce9e86b264fa5499f1408c45447bb6724dff8
Di Wu <wd1991610@gmail.com>
2018-04-14 18:26:19 -0700

    lint errors

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,15 +1,11 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Grid from "./homeGrid/CalendarGrid"
-import Sun from "./Sun"
+import React from 'react'
+import Grid from './homeGrid/CalendarGrid'
+import Sun from './Sun'
 
 class Home extends React.Component {
-constructor(props){
-  super(props);
-}
   render () {
     return (
-      //This will be where we add the Grid for the flowers
+      // This will be where we add the Grid for the flowers
       <React.Fragment>
         <div className="container" style={{ display: "block", width: "100%", height: "180px" }}>
           <Sun className="sun" />
@@ -18,10 +14,10 @@ constructor(props){
         </div>
         <h1>{this.props.userSession}</h1>
 
-        {(this.props.userSession!==null)?<Grid/>: ""}
+        {(this.props.userSession !== null) ? <Grid/> : ''}
 
       </React.Fragment>
-    );
+    )
   }
 }
 
